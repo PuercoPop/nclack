863bbca85dd015fce904be33108a37265c66cc17
Javier Olaechea <pirata@gmail.com>
2013-08-20 16:00:30 -0400

    read-http-line
      TODO probably wrap with unwind-protect to signal eof

--- src/packages.lisp
+++ src/packages.lisp
@@ -3,8 +3,12 @@
   (:import-from :alexandria :make-keyword)
   (:export :make-request))
 
-(defpackage :nclack.request
+(defpackage :nclack.utils
   (:use :cl))
+
+(defpackage :nclack.request
+  (:use :cl)
+  (:import-from :alexandria #:make-keyword))
 
 (defpackage :nclack.server
   (:use :cl :iolib))
---
+++ src/request/utils.lisp
@@ -0,0 +1,32 @@
+(in-package :nclack.request)
+
+(defun cl-rf ()
+  (format nil "~C~C" (code-char 13) (code-char 10)))
+
+(defun read-http-line (stream)
+  "read until crlf, return the string without the crlf."
+  (let ((line (make-array 140 :element-type 'character
+                          :adjustable t
+                          :fill-pointer 0)))
+    (read-http-line-iter stream line)))
+
+
+(defun read-http-line-iter (stream accum &optional prev-char)
+  (let ((current-char (read-char stream)))
+    (cond ((and (eq #\Linefeed current-char)
+                (eq #\Return prev-char)) accum)
+          ((eq #\Return current-char)
+           (read-http-line-iter stream
+                                accum 
+                                current-char))
+          (t
+           (read-http-line-iter stream
+                                (append-to-string accum current-char)
+                                current-char)))))
+
+(defun append-to-string (string &rest chars)
+  (loop for char in chars
+     do
+       (vector-push-extend char string)
+       )
+  string)
