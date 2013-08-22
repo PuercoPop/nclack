b5f085e199929cbb6de00d4200b7adb04ad95194
Javier Olaechea <pirata@gmail.com>
2013-08-22 14:05:54 -0400

    :(

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -1,1 +1,21 @@
 (in-package :nclack-tests)
+
+;; Design example
+;; (conforms-to '(:method :PUT
+;;                :request-uri "blah"
+;;                :server-protocol :HTTP/1.0))
+;; expands into
+;; (is (match result ((property :request-method x) (eq x :PUT))))
+;; (is (match result ((property :request-uri x)
+;;            (string= "/stuff/here?foo=bar"))))
+;; (is (match result ((property :server-protocol x) (eq x :HTTP/1.0))))
+(defmacro conforms-to (property-list)
+  `(progn ,@(loop for (key value) on property-list by #'cddr
+              collect
+                `(is (match result ((property ,key x) (eq x ,value)))))))
+
+;; Use case
+;; (let ((result (list :method :PUT :request-uri "blah" :server-protocol :HTTP/1.0)))
+;;   (conforms-to (:method :PUT
+;;                  :request-uri "blah"
+;;                  :server-protocol :HTTP/1.0)))
