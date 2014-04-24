566c2ac3e4ff0b6429e59e43d5630525d42bd7bd
PuercoPop <pirata@gmail.com>
2014-04-24 17:26:43 -0500

    Fix conforms-to macro

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -13,30 +13,30 @@
 present in a clack request and, if presemt in the pattern-request, compare the
 two values with the appropiate form. EQ for keywords, EQL for numbers, STRING=
 for strings and STRING= on the STREAM-TO-STRING for streams."
-  (when (getf pattern-request :request-method)
-    (is (eq (getf request-to-test :request-method)
+  (awhen (getf pattern-request :request-method)
+    (is (eq it
              (getf request-to-test :request-method))))
-  (when (getf pattern-request :script-name)
-    (is (string= (getf request-to-test :script-name)
+  (awhen (getf pattern-request :script-name)
+    (is (string= it
                   (getf request-to-test :script-name))))
-  (when (getf pattern-request :path-info)
-    (is (string= (getf request-to-test :path-info)
+  (awhen (getf pattern-request :path-info)
+    (is (string= it
                   (getf request-to-test :path-info))))
-  (when (getf pattern-request :query-string)
-    (is (string= (getf request-to-test :query-string)
+  (awhen (getf pattern-request :query-string)
+    (is (string= it
                   (getf request-to-test :query-string))))
-  (when (getf pattern-request :server-name)
-    (is (string= (getf request-to-test :server-name)
+  (awhen (getf pattern-request :server-name)
+    (is (string= it
                   (getf request-to-test :server-name))))
-  (when (getf pattern-request :server-port)
-    (is (eql (getf request-to-test :server-port)
+  (awhen (getf pattern-request :server-port)
+    (is (eql it
               (getf request-to-test :server-port))))
-  (when (getf pattern-request :server-protocol)
-    (is (eq (getf request-to-test :server-protocol)
+  (awhen (getf pattern-request :server-protocol)
+    (is (eq it
              (getf request-to-test :server-protocol))))
-  (when (getf pattern-request :request-uri)
-    (is (string= (getf request-to-test :request-uri)
+  (awhen (getf pattern-request :request-uri)
+    (is (string= it
                   (getf request-to-test :request-uri))))
-  (when (getf pattern-request :raw-body)
-    (is (string= (stream-to-string (getf request-to-test :raw-body))
+  (awhen (getf pattern-request :raw-body)
+    (is (string= (stream-to-string it)
                   (stream-to-string (getf request-to-test :raw-body))))))
